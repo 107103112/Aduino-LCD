48b42706d64e3b8e011668065a71c70e2fc3c6d2
107103112 <95265877+107103112@users.noreply.github.com>
2021-11-30 11:52:56 +0800

    changer to school number

--- dht/dht.ino
+++ dht/dht.ino
@@ -1,10 +1,10 @@
 #include <dht.h>     
-#define dht_dpin A0 	           //DHT11 類比輸入  
+#define dht_dpin A0 	           //DHT11 類比輸入 PIN A0  
 
 dht DHT;   
 
 String a;
-String Sch_no = "111";
+String Sch_no = "112";
 
 void setup(){  
   Serial.begin(9600);   
